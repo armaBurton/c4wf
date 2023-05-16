b70101250148bacaf88b0ca6fc11438fc88bd56b
Arma Burton <arma@armaburton.com>
2023-05-15 19:26:57 -0700

    error checking factorial

--- Program.Functions.cs
+++ Program.Functions.cs
@@ -84,14 +84,23 @@ partial class Program{
     } else if (number == 0){
       return 1;
     } else {
-      return number * Factorial(number - 1);
+      checked{
+        return number * Factorial(number - 1);
+      }
     }
+
+
   }
 
   static void RunFactorial(){
-    for (int i = 1; i <= 15; i++){
-      WriteLine($"{i}! = {Factorial(i)}");
+    for (int i = -2; i <= 15; i++){
+      try{
+        WriteLine($"{i}! = {Factorial(i):N0}");
+      } catch (OverflowException) {
+        WriteLine($"{i}! is too big for a 32-bit integer.");
+      } catch (Exception ex){
+        WriteLine($"{i}! throws {ex.GetType()}: {ex.Message}");
+      }
     }
   }
-
 }
