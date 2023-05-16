8d274ffbe365bd8ed97a017d588390a665e9733b
Arma Burton <arma@armaburton.com>
2023-05-15 18:53:23 -0700

    tax calculator

--- Program.Functions.cs
+++ Program.Functions.cs
@@ -6,4 +6,40 @@ partial class Program{
     }
     WriteLine($"");
   }
+
+  static decimal CalculateTax(
+    decimal amount,
+    string twoLetterRegionCode
+  ){
+    switch (twoLetterRegionCode){
+      case "CH": //Switzerland
+        rate = 0.08M;
+        break;
+      case "DK": //Denmark
+      case "NO": //Norway
+        rate = 0.25M;
+        break;
+      case "HU": //Hungary
+        rate = 0.27M;
+        break;
+      case "OR":
+      case "AK":
+      case "MT":
+        rate = 0.0M;
+        break;
+      case "ND":
+      case "WI":
+      case "ME":
+      case "VA":
+        rate = 0.05M;
+        break;
+      case "CA":
+        rate = 0.0825M;
+        break;
+      default:
+        rate = 0.06M;
+        break;
+    }
+    return amount * rate;
+  }
 }
